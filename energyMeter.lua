b7a3fe5151141947e6d4b0937086523d3d631033
pfory <pfory@seznam.cz>
2016-03-09 23:26:25 +0100

    v0.3

--- energyMeter.lua
+++ energyMeter.lua
@@ -1,118 +1,104 @@
 --Init  
-DeviceID="esp02"  
-RoomID="2"  
-Energy            = 0
+base = "/flat/EnergyMeter/esp09/"
+
+
 pulseTotal        = 0
-pulseHour         = 0
-pulseDay          = 0
-pulseCountkWh     = 700    --pocet impulsu na 1 kWh
-WhkWh             = 1000   --pocet Wh na kWh
-diffForSavekWh    = 1/10    --diference kWh pro zapis do EEPROM
-minuteOld         = 0
-hourOld           = 0
-Consumption       = 0
-cycles            = 0
+pulseLength       = 0
+pulseOld          = 0
+heartBeat         = 10
+lastSend          = tmr.now()
+pulseDuration     = 0
 
 wifi.setmode(wifi.STATION)
 wifi.sta.config("Datlovo","Nu6kMABmseYwbCoJ7LyG")
 
-Broker="213.192.58.66"  
+Broker="88.146.202.186"  
 
-pinLed = 4
+pinLed = 1
 gpio.mode(pinLed,gpio.OUTPUT)  
-gpio.write(pinLed,gpio.HIGH)  
+gpio.write(pinLed,gpio.LOW)  
 
-versionSW         = "0.1"
+versionSW         = 0.3
 versionSWString   = "EnergyMeter v" 
 print(versionSWString .. versionSW)
 
-sntp.sync('192.168.1.2', function(sec,usec,server)
-  print('Time sync ', sec, usec, server)
-end,
-function()
- print('Time sync failed!')
-end)
-
--- -- use pin 7 as the input pulse width counter
--- function pinPulse(level)
-  -- if level == gpio.LOW then
-    -- du = tmr.now() - pulse1
-    -- print(du)
-    -- gpio.write(pinLed,gpio.HIGH)  
-    -- pulseTotal=pulseTotal+1
-    -- pulseDay=pulseDay+1
-    -- pulseHour=pulseHour+1
-  -- else
-    -- pulse1 = tmr.now()
-    -- gpio.write(pinLed,gpio.LOW)  
-  -- end
--- end
-
-
-pin = 7
+pin = 2
 pulse1 = 0
 du=0
 gpio.mode(pin,gpio.INT)
+
 function pinPulse(level)
-  du = tmr.now() - pulse1
-  print(du)
-  pulse1 = tmr.now()
-  if level == gpio.HIGH then 
-    gpio.trig(pin, "down") 
-    gpio.write(pinLed,gpio.LOW)  
-  else 
-    gpio.trig(pin, "up") 
-    pulseTotal=pulseTotal+1
-    pulseDay=pulseDay+1
-    pulseHour=pulseHour+1  
-    consumption = consumption + 3600000/(tmr.now()/1000 - lastPulse)
-    cycles=cycles+1
-    lastPulse=tmr.now()    
+  if gpio.read(pin) == gpio.HIGH then
+  --if level == gpio.HIGH then --nabezna hrana
+    --tmr.delay(10000)
+    pulseDuration = tmr.now()
     gpio.write(pinLed,gpio.HIGH)  
+    print("nabezna")
+  else 
+    if (tmr.now() - pulseDuration) > 70000 and (tmr.now() - pulseDuration) < 100000 then
+      pulseTotal=pulseTotal+1
+      gpio.write(pinLed,gpio.LOW) 
+      if (pulseLength<0) then --timer overloaded
+        pulseLength = pulseLength + 2147483648
+      end
+      pulseLength = (tmr.now() - pulseOld)/1000
+      pulseOld = tmr.now()
+      print("dobezna")
+      print(pulseLength)
+      sendData()
+    end
   end
 end
-gpio.trig(pin, "down", pinPulse)
 
-tmr.alarm(2, 10000, 1, function() 
-  print("I am sending data to OpenHab")
-  Energy = pulse2kWh(pulseTotal)
-  print(Energy)
-  if cycles>0 then
-    m:publish("/home/".. RoomID .."/" .. DeviceID .. "/p1/Consumption",Consumption/cycles,0,0)  
-    cycles=0
-    Consumption=0
+
+function sendData()
+  if (tmr.now() - lastSend) > 5000000 or lastSend > tmr.now() then
+    lastSend = tmr.now()
+    print("I am sending pulse to OpenHab")
+    print(pulseTotal)
+    --pulseTotal = pulseTotal + 1
+    --pulseLength = math.random(100,1000000)
+    m:publish(base.."Pulse",pulseTotal,0,0)  
+    m:publish(base.."pulseLength", pulseLength,0,0)  
+    m:publish(base.."VersionSW",   versionSW,0,0)  
+    m:publish(base.."HeartBeat",   heartBeat,0,0)
+   
+    file.open("config.ini", "w+")
+    --file.write('pulseCount=')
+    file.write(string.format("%u", pulseTotal))
+    file.write("\n\r")
+    file.close()
+    
+    if heartBeat==0 then heartBeat=1
+    else heartBeat=0
+    end
+  end 
+end
+
+function reconnect()
+  print ("Waiting for Wifi")
+  if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
+    print ("Wifi Up!")
+    tmr.stop(1) 
+    m:connect(Broker, 31883, 0, function(conn) 
+      print(wifi.sta.getip())
+      print("Mqtt Connected to:" .. Broker) 
+      mqtt_sub() --run the subscription function 
+    end)
   end
-  m:publish("/home/".. RoomID .."/" .. DeviceID .. "/p1/Energy",Energy,0,0)  
- -- if (minute() < minuteOld) {
-    -- minuteOld=0;
-    -- pulseHour=0;
-  -- }
-  -- if (hour() < hourOld) {
-    -- hourOld=0;
-    -- pulseDay=0;
-  -- }
-end)
+end
 
--- tmr.alarm(3, 100, 1, function() 
-  -- pulseTotal=pulseTotal+1
--- end)
 
-m = mqtt.Client("ESP8266".. DeviceID, 180, "datel", "hanka")  
+m = mqtt.Client("ESP8266 PowerMeter", 180, "datel", "hanka12")  
 m:lwt("/lwt", "ESP8266", 0, 0)  
 m:on("offline", function(con)   
-  print ("Mqtt Reconnecting....")   
-  tmr.alarm(1, 10000, 0, function()  
-    m:connect(Broker, 31883, 0, function(conn)   
-      print("Mqtt Connected to:" .. Broker)  
-      mqtt_sub() --run the subscription function  
-    end)  
-  end)  
+  print ("Mqtt Reconnecting...")   
+  tmr.alarm(1, 10000, 1, function()  
+    reconnect()
+  end)
 end)  
 
 function mqtt_sub()  
-  m:subscribe("/home/".. RoomID .."/" .. DeviceID .. "/p1/Energy",0, function(conn)   
-    print("Mqtt Subscribed to OpenHAB feed for device " .. DeviceID .. "(EnergyMeter)")  
-  end)  
 end  
 
  -- on publish message receive event  
@@ -120,26 +106,27 @@ m:on("message", function(conn, topic, data)
   print("Received:" .. topic .. ":" .. data) 
 end)  
 
-tmr.alarm(0, 1000, 1, function()  
-  if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then  
-    tmr.stop(0)  
-    m:connect(Broker, 31883, 0, function(conn)   
-      print("Mqtt Connected to:" .. Broker)  
-      mqtt_sub() --run the subscription function  
---      tmr.alarm(1, 5000, 1, function() 
---      end)
-    end)
+tmr.alarm(0, 1000, 1, function() 
+  print ("Connecting to Wifi... ")
+  if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
+    print ("Wifi connected")
+    tmr.stop(0) 
+    print ("Read config file... ")
+    file.open("config.ini", "r")
+    s = file.readline()
+    if (s==nil) then
+      print("empty file")
+      pulseTotal=0
+    else
+      pulseTotal = s
+    end
+    print(pulseTotal)
+    file.close()  
+    m:connect(Broker, 31883, 0, function(conn) 
+      print(wifi.sta.getip())
+      print("Mqtt Connected to:" .. Broker.." - "..base) 
+      gpio.trig(pin, "both", pinPulse)
+      --tmr.alarm(2, 2000, 1, function() sendData() end )
+    end) 
   end
 end)
-
-function pulse2kWh(pulse) 
-  return pulse/pulseCountkWh
-end
-
-function kWh2Pulse(kWh)
-  return kWh*pulseCountkWh
-end
-
-function Wh2kWh(Wh) 
-  return Wh/WhkWh
-end
