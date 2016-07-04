6d12d25cec69d161c88c83a1e822e51d3807a6ba
pfory <pfory@seznam.cz>
2016-07-04 18:47:44 +0200

    v0,7 na lua 151 4.7.2016 na chatu

--- energyMeterChata.lua
+++ energyMeterChata.lua
@@ -5,17 +5,21 @@ deviceID = "ESP8266 PowerMeter "..node.chipid()
 pulseTotal        = 0
 pulseLength       = 0
 pulseOld          = 0
-heartBeat         = node.bootreason() + 10
 lastSend          = tmr.now()
 pulseDuration     = 0
+pulseDurationMin  = 70000
+pulseDurationMax  = 100000
+sendingData       = false
+sendDelay         = 20000000 --in us
 
 heartBeat = node.bootreason() + 10
-print("Boot reason:")
+val = rtcmem.read32(41, 1) -- Read the values in slots 41
+if (val==20) then
+  heartBeat=val
+  rtcmem.write32(41, 0)
+end
+uart.write(0,"Boot reason:")
 print(heartBeat)
-
-wifi.setmode(wifi.STATION)
-wifi.sta.config("Datlovo","Nu6kMABmseYwbCoJ7LyG")
-wifi.sta.autoconnect(1)
 
 Broker="88.146.202.186"  
 
@@ -23,8 +27,8 @@ pinLed = 3
 gpio.mode(pinLed,gpio.OUTPUT)  
 gpio.write(pinLed,gpio.LOW)  
 
-versionSW         = 0.45
-versionSWString   = "EnergyMeter chata v" 
+versionSW         = 0.7
+versionSWString   = "EnergyMeter v" 
 print(versionSWString .. versionSW)
 
 pin = 4
@@ -33,15 +37,26 @@ du=0
 gpio.mode(pin,gpio.INT)
 
 function pinPulse(level)
-  if gpio.read(pin) == gpio.HIGH then
+  if gpio.read(pin) == gpio.LOW then
   --if level == gpio.HIGH then --nabezna hrana
     --tmr.delay(10000)
     pulseDuration = tmr.now()
     gpio.write(pinLed,gpio.HIGH)  
     print("nabezna")
   else 
-    --if (tmr.now() - pulseDuration) > 70000 and (tmr.now() - pulseDuration) < 100000 then
+    if (tmr.now() - pulseDuration) > pulseDurationMin and (tmr.now() - pulseDuration) < pulseDurationMax then
       pulseTotal=pulseTotal+1
+
+      if pulseTotal % 100 == 0 then
+        print("Save pulseTotal:"..pulseTotal)
+        file.open("config.ini", "w+")
+        file.write(string.format("%u", pulseTotal))
+        file.write("\n\r")
+        file.close()
+      end
+      rtcmem.write32(40, pulseTotal) --save pulsetotal to memory on address 40
+      print("Heap:"..node.heap())
+
       gpio.write(pinLed,gpio.LOW) 
       if (tmr.now()<pulseOld) then --timer overloaded
         pulseLength = (math.pow (2, 31) - pulseOld + tmr.now()) / 1000
@@ -49,41 +64,42 @@ function pinPulse(level)
         pulseLength = (tmr.now() - pulseOld)/1000
       end
       pulseOld = tmr.now()
-      print("dobezna")
-      print(pulseLength)
-      sendData()
-    --end
+      print("dobezna delka:"..pulseLength)
+      if (sendingData==false) then
+        sendData()
+      end
+    end
   end
 end
 
+function sendHB() 
+  m:publish(base.."VersionSW",   versionSW,0,0)  
+  m:publish(base.."HeartBeat",   string.format("%.0f",heartBeat),0,0)
+  if heartBeat==0 then heartBeat=1
+  else heartBeat=0
+  end
+end
 
 function sendData()
-  if (tmr.now() - lastSend) > 5000000 or lastSend > tmr.now() then
+  sendingData = true
+  if (tmr.now() - lastSend) > sendDelay or lastSend > tmr.now() then
     lastSend = tmr.now()
-    print("I am sending pulse to OpenHab")
-    print(pulseTotal)
+    print("I am sending pulse to OpenHab:"..pulseTotal)
     --pulseTotal = pulseTotal + 1
     --pulseLength = math.random(100,1000000)
-    m:publish(base.."Pulse",pulseTotal,0,0)  
-    m:publish(base.."pulseLength", pulseLength,0,0)  
-    m:publish(base.."VersionSW",   versionSW,0,0)  
-    m:publish(base.."HeartBeat",   heartBeat,0,0)
+    
+    m:publish(base.."Pulse",string.format("%.0f",pulseTotal),0,0)  
+    m:publish(base.."pulseLength", string.format("%.0f",pulseLength),0,0)  
 
-    if pulseTotal % 100 == 0 then
-      file.open("config.ini", "w+")
-      --file.write('pulseCount=')
-      file.write(string.format("%u", pulseTotal))
-      file.write("\n\r")
-      file.close()
-    end
-    if heartBeat==0 then heartBeat=1
-    else heartBeat=0
-    end
+    print("Data sent")
+
   end 
+  sendHB()
+  sendingData = false
 end
 
 function mqtt_sub()  
-  m:subscribe(base,0, function(conn)   
+  m:subscribe(base.."com",0, function(conn)   
     print("Mqtt Subscribed to OpenHAB feed for device "..deviceID)  
   end)  
 end  
@@ -94,7 +110,7 @@ function reconnect()
   if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
     print ("Wifi Up!")
     tmr.stop(1) 
-    m:connect(Broker, 31883, 0, function(conn) 
+    m:connect(Broker, 31883, 0, 1, function(conn) 
       print(wifi.sta.getip())
       print("Mqtt Connected to:" .. Broker) 
       mqtt_sub() --run the subscription function 
@@ -119,34 +135,41 @@ m:on("message", function(conn, topic, data)
   if topic == base.."com" then
     if data == "ON" then
       print("Restarting ESP, bye.")
+      rtcmem.write32(41, 20) --save bootreason 20 to memory on address 41
       node.restart()
     end
   end
 end)  
 
-tmr.alarm(0, 1000, 1, function() 
-  print ("Connecting to Wifi... ")
-  if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
-    print ("Wifi connected")
-    tmr.stop(0) 
-    m:connect(Broker, 31883, 0, function(conn) 
-      mqtt_sub() --run the subscription function 
-      print(wifi.sta.getip())
-      print("Mqtt Connected to:" .. Broker.." - "..base) 
-      gpio.trig(pin, "both", pinPulse)
-      print ("Read config file... ")
-      file.open("config.ini", "r")
-      s = file.readline()
-      if (s==nil) then
-        print("empty file")
-        pulseTotal=0
-      else
-        pulseTotal = s
-      end
-      print(pulseTotal)
-      file.close()  
-      m:publish(base.."VersionSW",   versionSW,0,0)  
-      m:publish(base.."HeartBeat",   heartBeat,0,0)
-    end) 
+function readConfig() 
+  print ("Read config file... ")
+  file.open("config.ini", "r")
+  s = file.readline()
+  if (s==nil) then
+    print("empty file")
+    pulseTotal=0
+  else
+    pulseTotal = s
+    pulseTotal=pulseTotal+0
   end
-end)
+  file.close()  
+  print("PulseTotal from file:"..pulseTotal)
+  val = rtcmem.read32(40, 1) -- Read the values in slots 41
+  print("PulseTotal from EEPROM:"..val)
+  if (val - pulseTotal<100) then
+    pulseTotal = val
+    print("Correct pulseTotal from EEPROM:"..pulseTotal)
+  end
+  rtcmem.write32(40, pulseTotal) --save pulsetotal to memory on address 40
+end
+
+readConfig()
+
+m:connect(Broker, 31883, 0, 1, function(conn) 
+  mqtt_sub() --run the subscription function 
+  sendHB()
+  print(wifi.sta.getip())
+  print("Mqtt Connected to:" .. Broker.." - "..base) 
+  gpio.trig(pin, "both", pinPulse)
+end) 
+
--- init.lua
+++ init.lua
@@ -1,3 +1,34 @@
-print("Wait 5 seconds please")
-tmr.alarm(0, 5000, 0, function() dofile('energyMeter.lua') end)
---tmr.alarm(0, 2000, 0, function() dofile('energyMeterChata.lua') end)
+wifi.setmode(wifi.STATION)
+wifi.sta.config("Datlovo","Nu6kMABmseYwbCoJ7LyG")   ---   SSID and Password for your LAN DHCP here
+
+cfg={
+  ip = "192.168.1.158", 
+  netmask = "255.255.255.0",
+  gateway = "192.168.1.1"
+}
+wifi.sta.setip(cfg)
+wifi.sta.autoconnect(1)
+
+
+uart.write(0,"Connecting to Wifi")
+tmr.alarm(0, 1000, 1, function() 
+  uart.write(0,".")
+  if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
+    tmr.stop(0) 
+    print ("Wifi connected")
+    print("System Info:  ")
+    print("IP: ")
+    print(wifi.sta.getip())
+    majorVer, minorVer, devVer, chipid, flashid, flashsize, flashmode, flashspeed = node.info();
+    print("NodeMCU "..majorVer.."."..minorVer.."."..devVer.."\nFlashsize: "..flashsize.."\nChipID: "..chipid)
+    print("FlashID: "..flashid.."\n".."Flashmode: "..flashmode.."\nHeap: "..node.heap())
+     -- get file system info
+    remaining, used, total=file.fsinfo()
+    print("\nFile system info:\nTotal : "..total.." Bytes\nUsed : "..used.." Bytes\nRemain: "..remaining.." Bytes")
+    print("\nReady")
+    dofile("servernode.lua")    --  calls servernode.lua  
+    dofile("energyMeterChata.lua")    --  calls servernode.lua  
+  end
+end)
+
+    
