a563ee552dd8b0b9534639ee10023ba550136b3e
pfory <pfory@seznam.cz>
2021-12-12 20:05:58 +0100

    +

--- energyMeter.ino
+++ energyMeter.ino
@@ -124,7 +124,10 @@ bool reconnect(void *) {
     DEBUG_PRINT("Attempting MQTT connection...");
     // Attempt to connect
     if (client.connect(mqtt_base, mqtt_username, mqtt_key, (String(mqtt_base) + "/LWT").c_str(), 2, true, "offline", true)) {
-      client.subscribe((String(mqtt_base) + "/#").c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_topic_restart)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_topic_netinfo)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_config_portal_stop)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_config_portal)).c_str());
       client.publish((String(mqtt_base) + "/LWT").c_str(), "online", true);
      DEBUG_PRINTLN("connected");
     } else {
